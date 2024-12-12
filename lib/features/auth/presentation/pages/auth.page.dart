68b08c7503825a30f5a0a1396ab6abb5ab57ee37
Jônatas Souza de Paula <207073@pjro.local>
2024-12-12 16:26:07 -0400

    authpage

--- lib/features/auth/presentation/pages/auth.page.dart
+++ lib/features/auth/presentation/pages/auth.page.dart
@@ -2,17 +2,16 @@ import 'package:emeron/core/utils/constants/image.constants.dart';
 import 'package:emeron/core/utils/widgets/default_stack.widgets.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
-import 'package:emeron/core/utils/constants/constants.dart';
-import 'package:emeron/core/utils/widgets/default_stack.widgets.dart';
-import 'package:emeron/features/auth/presentation/controllers/auth.controller.dart';
+import 'package:emeron/features/home/presentation/controllers/home.controller.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class AuthPage extends GetView<AuthController> {
+class AuthPage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: DefaultStack(
         child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
+            padding: const EdgeInsets.all(24),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -21,14 +20,59 @@ class AuthPage extends GetView<AuthController> {
                   child: Image.asset(AppImages.logo),
                 ),
                 Container(
-                  color: Colors.white,
-                  margin: EdgeInsets.symmetric(vertical: 16),
                   width: 286,
-                  height: 286,
+                  margin: const EdgeInsets.symmetric(vertical: 16),
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(.25),
+                    borderRadius: BorderRadius.circular(19),
+                  ),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Login',
+                        style: GoogleFonts.roboto(
+                            color: Colors.white, fontSize: 26),
+                      ),
+                      const Divider(),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: TextField(
+                          decoration: getInputDecoration(),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: TextField(
+                          decoration: getInputDecoration(),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: OutlinedButton(
+                            style: const ButtonStyle(
+                                minimumSize: WidgetStatePropertyAll(
+                                    Size(double.infinity, 50)),
+                                foregroundColor:
+                                    WidgetStatePropertyAll(Colors.white),
+                                backgroundColor:
+                                    WidgetStatePropertyAll(Color(0xFF13385D))),
+                            onPressed: () {},
+                            child: Text('Logar')),
+                      )
+                    ],
+                  ),
                 )
               ],
             )),
       ),
     );
+  }
+
+  InputDecoration getInputDecoration() {
+    return InputDecoration(
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)));
   }
 }
