b341ecc751114dc49a0cdaf529793d5ba0ce2e36
Jônatas Souza de Paula <207073@pjro.local>
2024-12-12 11:38:43 -0400

    default stack

--- lib/features/auth/presentation/pages/auth.page.dart
+++ lib/features/auth/presentation/pages/auth.page.dart
@@ -1,3 +1,5 @@
+import 'package:emeron/core/utils/constants/image.constants.dart';
+import 'package:emeron/core/utils/widgets/default_stack.widgets.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:emeron/features/home/presentation/controllers/home.controller.dart';
@@ -5,6 +7,12 @@ import 'package:emeron/features/home/presentation/controllers/home.controller.da
 class AuthPage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: DefaultStack(
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
   }
 }
--- lib/features/calendar/presentation/pages/calendar.page.dart
+++ lib/features/calendar/presentation/pages/calendar.page.dart
@@ -1,3 +1,5 @@
+import 'package:emeron/core/utils/constants/image.constants.dart';
+import 'package:emeron/core/utils/widgets/default_stack.widgets.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:emeron/features/calendar/presentation/controllers/calendar.controller.dart';
@@ -5,6 +7,12 @@ import 'package:emeron/features/calendar/presentation/controllers/calendar.contr
 class CalendarPage extends GetView<CalendarController> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Material(
+      child: DefaultStack(
+        child: Center(
+          child: Image.asset(AppImages.logo),
+        ),
+      ),
+    );
   }
 }
